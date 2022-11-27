c11b11f2bbfc43827ea915e1701318f0282ac56a
matthew-via-music <mvm@matthewviamusic.com>
2022-11-27 23:18:01 +0000

    add conditional responsive layout with matchMedia

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,47 @@
+import { useState, useEffect } from 'react';
 import './App.css'
 import GroupAStats from './components/GroupAStats'
 import GroupAMatches from './components/GroupAMatches'
 function App() {
+  
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 2336px)").matches
+  )
+
+  useEffect(() => {
+    window
+    .matchMedia("(min-width: 2336px)")
+    .addEventListener('change', e => setMatches( e.matches ))
+  }, [])
 
   return (
     <>
      <h1>World Cup 2022</h1>
-
      <div className="container">
 
-      <div>
-        <h2>Group A</h2>
-        <GroupAStats />
-      </div>
-      <div>
-        <h2>Group B</h2>
-        <GroupAStats />
-      </div>
-      <div>
-        <h2>Group C</h2>
-        <GroupAStats />
-      </div>
+      {matches && 
+        <>
+          <div><h2>Group A</h2><GroupAStats /></div>
+          <div><h2>Group B</h2><GroupAStats /></div>
+          <div><h2>Group C</h2><GroupAStats /></div>
+          <div><h2>Group A Matches</h2><GroupAMatches /></div>
+          <div><h2>Group B Matches</h2><GroupAMatches /></div>
+          <div><h2>Group C Matches</h2><GroupAMatches /></div>
+        </>
+      }
+      {!matches && 
+        <>
+          <div><h2>Group A</h2><GroupAStats /></div>
+          <div><h2>Group A Matches</h2><GroupAMatches /></div>
+          <div><h2>Group B</h2><GroupAStats /></div>
+          <div><h2>Group B Matches</h2><GroupAMatches /></div>
+          <div><h2>Group C</h2><GroupAStats /></div>
+          <div><h2>Group C Matches</h2><GroupAMatches /></div>
+        </>
+      }
           
-      <div>
-        <h2>Group A Matches</h2>
-        <GroupAMatches />
-      </div>
-      <div>
-        <h2>Group B Matches</h2>
-        <GroupAMatches />
-      </div>
-      <div>
-        <h2>Group C Matches</h2>
-        <GroupAMatches />
-      </div>
 
+     
       {/* \container */}
       </div>
     </>
