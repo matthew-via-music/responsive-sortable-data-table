137d66b9eed9856e01eae42e855c00da3cbe2f1d
matthew-via-music <mvm@matthewviamusic.com>
2022-11-29 00:35:33 +0000

    configure 'super screen pivot' option + scaffold data files

--- src/App.tsx
+++ src/App.tsx
@@ -6,47 +6,128 @@ import GroupMatches from './components/GroupMatches'
 import Buttons from './components/Buttons'
 
 import GroupAStatsData from "./data/groupAStats.json"
+import GroupBStatsData from "./data/groupBStats.json"
+import GroupCStatsData from "./data/groupCStats.json"
+import GroupDStatsData from "./data/groupDStats.json"
+import GroupEStatsData from "./data/groupEStats.json"
+import GroupFStatsData from "./data/groupFStats.json"
+import GroupGStatsData from "./data/groupGStats.json"
+import GroupHStatsData from "./data/groupHStats.json"
+
 import GroupAMatchesData from "./data/groupAMatches.json"
+import GroupBMatchesData from "./data/groupBMatches.json"
+import GroupCMatchesData from "./data/groupCMatches.json"
+import GroupDMatchesData from "./data/groupDMatches.json"
+import GroupEMatchesData from "./data/groupEMatches.json"
+import GroupFMatchesData from "./data/groupFMatches.json"
+import GroupGMatchesData from "./data/groupGMatches.json"
+import GroupHMatchesData from "./data/groupHMatches.json"
 
 function App() {
   
-  const [UILayoutFormat, setUILayoutFormat] = useState(
-    window.matchMedia("(min-width: 2336px)").matches
+  const [superScreenView, setSuperScreenView] = useState(
+    window.matchMedia("(min-width: 2336px)").matches && true
   )
   const [toggleGroups, setToggleGroups] = useState(false)
   const [toggleMatches, setToggleMatches] = useState(false)
+  
+  // 
+  const [toggleSuperScreen, setToggleSuperScreen] = useState(false)
 
+  function toggleSuperScreenFn(){
+    setToggleSuperScreen((x: any) => !x)
+    setSuperScreenView((x: any) => !x)
+  }
+  // 
+  
   useEffect(() => {
     window
     .matchMedia("(min-width: 2336px)")
-    .addEventListener('change', e => setUILayoutFormat( e.matches ))
+    .addEventListener('change', e => setSuperScreenView( e.matches ))
   }, [])
 
   return (
     <>
      <h1>World Cup 2022</h1>
-    
-     <Buttons toggleGroups={toggleGroups} toggleMatches={toggleMatches} setToggleGroups={setToggleGroups} setToggleMatches={setToggleMatches} />
+
+     <Buttons 
+      toggleGroups={toggleGroups} setToggleGroups={setToggleGroups} 
+      toggleMatches={toggleMatches} setToggleMatches={setToggleMatches} 
+      toggleSuperScreen={toggleSuperScreen} toggleSuperScreenFn={toggleSuperScreenFn} 
+      />
 
      <div className="container">
-      {UILayoutFormat && 
+      {superScreenView && 
         <>
-          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="A" GroupStatsData={GroupAStatsData}/></div>
-          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="B" GroupStatsData={GroupAStatsData}/></div>
-          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="C" GroupStatsData={GroupAStatsData}/></div>
-          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="A" GroupMatchesData={GroupAMatchesData} /></div>
-          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="B" GroupMatchesData={GroupAMatchesData} /></div>
-          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="C" GroupMatchesData={GroupAMatchesData} /></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="A" 
+          GroupStatsData={GroupAStatsData}/></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="B" 
+          GroupStatsData={GroupBStatsData}/></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="C" 
+          GroupStatsData={GroupCStatsData}/></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="D" 
+          GroupStatsData={GroupDStatsData}/></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="E" 
+          GroupStatsData={GroupEStatsData}/></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="F" 
+          GroupStatsData={GroupFStatsData}/></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="G" 
+          GroupStatsData={GroupGStatsData}/></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="H" 
+          GroupStatsData={GroupHStatsData}/></div>
+
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="A" 
+          GroupMatchesData={GroupAMatchesData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="B" 
+          GroupMatchesData={GroupBMatchesData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="C" 
+          GroupMatchesData={GroupCMatchesData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="D" 
+          GroupMatchesData={GroupDMatchesData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="E" 
+          GroupMatchesData={GroupEMatchesData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="F" 
+          GroupMatchesData={GroupFMatchesData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="G" 
+          GroupMatchesData={GroupGMatchesData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="H" 
+          GroupMatchesData={GroupHMatchesData} /></div>
         </>
       }
-      {!UILayoutFormat && 
+      {!superScreenView && 
         <>
-          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="A" GroupStatsData={GroupAStatsData}/></div>
-          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="A" GroupMatchesData={GroupAMatchesData} /></div>
-          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="B" GroupStatsData={GroupAStatsData} /></div>
-          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="B" GroupMatchesData={GroupAMatchesData} /></div>
-          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="C" GroupStatsData={GroupAStatsData} /></div>
-          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="C" GroupMatchesData={GroupAMatchesData} /></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="A" 
+          GroupStatsData={GroupAStatsData}/></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="A" 
+          GroupMatchesData={GroupAMatchesData} /></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="B" 
+          GroupStatsData={GroupBStatsData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="B" 
+          GroupMatchesData={GroupBMatchesData} /></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="C" 
+          GroupStatsData={GroupCStatsData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="C" 
+          GroupMatchesData={GroupCMatchesData} /></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="D" 
+          GroupStatsData={GroupDStatsData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="D" 
+          GroupMatchesData={GroupDMatchesData} /></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="E" 
+          GroupStatsData={GroupEStatsData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="E" 
+          GroupMatchesData={GroupEMatchesData} /></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="F" 
+          GroupStatsData={GroupFStatsData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="F" 
+          GroupMatchesData={GroupFMatchesData} /></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="G" 
+          GroupStatsData={GroupGStatsData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="G" 
+          GroupMatchesData={GroupGMatchesData} /></div>
+          <div className={`${toggleGroups ? "toggleHideGroups" : ""}`}><GroupStats Group="H" 
+          GroupStatsData={GroupHStatsData} /></div>
+          <div className={`${toggleMatches ? "toggleHideMatches" : ""}`}><GroupMatches Group="H" 
+          GroupMatchesData={GroupHMatchesData} /></div>
         </>
       }
       {/* \container */}
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -2,16 +2,21 @@ interface Props {
   toggleGroups?: any,
   toggleMatches?: any,
   setToggleGroups?: any,
-  setToggleMatches?: any
+  setToggleMatches?: any,
+  toggleSuperScreen?:any,
+  toggleSuperScreenFn?:any,
+  // setToggleSuperScreen?:any
 }
 
 function Butons(props: Props) {
-  const {toggleGroups, toggleMatches, setToggleGroups, setToggleMatches} = props
+  const {toggleGroups, toggleMatches, setToggleGroups, setToggleMatches, toggleSuperScreen, toggleSuperScreenFn} = props
 
   return (
     <div className="container collapse">
        <button className={toggleGroups ? "showBtn" : "hideBtn"} onClick={() => setToggleGroups((x: any) => !x)}>{toggleGroups ? "Show Groups" : "Hide Groups"}</button>
        <button className={toggleMatches ? "showBtn" : "hideBtn"} onClick={() => setToggleMatches((x: any) => !x)}>{toggleMatches ? "Show Matches" : "Hide Matches"}</button>
+
+       <button id="superScreenBtn" className={toggleSuperScreen ? "showBtn" : "hideBtn"} onClick={() => toggleSuperScreenFn()}>{toggleSuperScreen ? "Super Screen" : "Pivot Screen"}</button>       
      </div>
   )
 }
