f4989b750b99fcd1b29beefdc6ce58af1a7a2f77
matthew-via-music <mvm@matthewviamusic.com>
2023-03-05 20:48:58 +0000

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/responsive-sortable-data-table',
+  base: '/responsive-sortable-data-table/',
   plugins: [react()]
 })
