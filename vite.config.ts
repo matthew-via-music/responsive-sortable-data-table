325743f9e24d3f57f36a4414a7f16d313c80fd7d
matthew-via-music <mvm@matthewviamusic.com>
2023-03-05 02:10:32 +0000

    correct base path + deploy script directory name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/responsive-sortable-data-table',
   plugins: [react()]
 })
